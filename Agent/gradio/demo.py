a7afb19b890139860ea138d75c49e5062e5a352f
kidoom <3416888646@qq.com>
2024-07-09 22:41:49 +0800

    功能完善

--- Agent/gradio/demo.py
+++ Agent/gradio/demo.py
@@ -16,19 +16,24 @@ chatbot = gr.Chatbot(
     layout="bubble",  # 或 "panel"，根据需要选择布局
 )
 
+agent_button = gr.Button("发送")
+
 with gr.Blocks() as agent:
     with gr.Tab(label="chat"):
-        chat = gr.ChatInterface(predict, chatbot=chatbot)
+
+        chat = gr.ChatInterface(predict, chatbot=chatbot,submit_btn = agent_button)
     with gr.Tab(label="作文评分"):
         with gr.Row():
             with gr.Column():
                 file = gr.File(type="filepath")
                 OCR_text = gr.Textbox(label="识别结果", show_copy_button=True)
-                OCR_button = gr.Button()
+                OCR_button = gr.Button("开始识别")
                 OCR_button.click(fn=OCRTool, inputs=file, outputs=OCR_text)
             with gr.Column():
                 txt1 = gr.Textbox(label="命题标准", max_lines=10)
                 txt2 = gr.Textbox(label="作文内容", max_lines=15)
+        with gr.Row():
+            with gr.Column():
                 result = gr.Textbox(label="批改结果", lines=1, interactive=False, show_copy_button=True)
                 b = gr.Button("开始批改")
                 b.click(RateArticleTool, inputs=[txt1, txt2], outputs=result)
